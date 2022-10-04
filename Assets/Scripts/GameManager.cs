969559bc59047c65a8b55491e2113158c5bca00c
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2022-10-04 14:36:32 -0700

    Continued working on Garage

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -30,8 +31,9 @@ public class GameManager : MonoBehaviour
         Application.Quit();
     }
 
-    public void OpenScene(int SceneNum)
+    public void OpenScene(int sceneNum)
     {
-        Debug.Log("It worked!");
+        //Debug.Log("It worked!");
+        SceneManager.LoadScene(sceneNum);
     }
 }
